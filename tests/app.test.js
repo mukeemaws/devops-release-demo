42551b004b2948f3b5b2d1e9c000e8f0614f4b16
Ubuntu <ubuntu@ip-172-31-32-237.ec2.internal>
2026-08-03 05:45:35 +0000

    feat: add health endpoint

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,7 +6,15 @@ describe("GET /", () => {
         const res = await request(app).get("/");
 
         expect(res.statusCode).toBe(200);
-
         expect(res.body.message).toBe("Hello DevOps!");
+    });
+});
+
+describe("GET /health", () => {
+    it("should return status UP", async () => {
+        const res = await request(app).get("/health");
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.status).toBe("UP");
     });
 });
